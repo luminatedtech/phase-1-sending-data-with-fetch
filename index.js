22ee1ad3123b406b11c3006a5e15057ac2c35a9f
Allen <avalmercafe02@gmail.com>
2022-06-14 23:47:29 -0400

    commit tree 575a9e7ec719967b99b4e5f54972e535e42f38e4

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+
+const formData = {
+    name: "Steve",
+    email: "steve@steve.com",
+  };
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body:JSON.stringify(formData)
+
+
+  };
+
+function submitData (name, email) {
+
+      fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (id){
+          console.log(id[1])
+      })}
